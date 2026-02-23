d83d261246c6f4b24365b09eb00725ee7066c157
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2026-02-23 21:46:44 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  output: 'export', // static export
   trailingSlash: true, // чтобы ссылки были корректны
   basePath: isProd ? '/shovel' : '', // обязательно для GH Pages
   assetPrefix: isProd ? '/shovel/' : '',
