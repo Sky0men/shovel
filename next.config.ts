0ebd00614f722d5b5fae62de02eee46ab80c3be8
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2026-02-23 21:41:41 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   output: 'export', // static export
   trailingSlash: true, // чтобы ссылки были корректны
   basePath: isProd ? '/shovel' : '', // обязательно для GH Pages
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  assetPrefix: isProd ? '/shovel/' : '',
   images: {
     unoptimized: true, // чтобы next/image не ломался
   },
