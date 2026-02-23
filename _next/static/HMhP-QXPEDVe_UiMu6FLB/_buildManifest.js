c5f5937ca9da2acff396fa192012207d556c36cc
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2026-02-23 17:43:43 +0300

    Updates

--- _next/static/HMhP-QXPEDVe_UiMu6FLB/_buildManifest.js
+++ _next/static/HMhP-QXPEDVe_UiMu6FLB/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shovel//_next/:path+",
+        "destination": "/shovel/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
