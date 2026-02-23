ed04b566474828e82b4ccc2839e90f8015756f2a
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2026-02-23 17:30:27 +0300

    fix configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // эта опция заменяет next export
+  // Можно добавить basePath, если сайт не в корне домена GitHub Pages
+  basePath: '/shovel', // только если пушишь в репозиторий как https://username.github.io/shovel/
 };
 
 export default nextConfig;
