2149cc65b2ecb880bc64aafd67f9d0a296d80d8a
Ivan Zelentsov <vanya.zelenczov@mail.ru>
2026-02-23 17:57:29 +0300

    Updates

--- _next/static/wna6Jm0IU9IoLu2chNhFf/_buildManifest.js
+++ _next/static/wna6Jm0IU9IoLu2chNhFf/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/shovel//_next/:path+",
-        "destination": "/shovel/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
